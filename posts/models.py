50d77e1fec1853a53e042a103331edd60a1491bb
Anonimous05 <airastolonov05@gmail.com>
2022-09-16 19:54:53 +0600

    test

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+POST_TYPE_CHOICES = (
+    (1, 'Animals'),
+    (2, 'Cars'),
+    (3, 'Recipes'),
+    (4, 'Nature'),
+    (5, 'Other')
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True, blank=True, null=True)
+    stars = models.IntegerField(null=True)
+    type = models.IntegerField(choices=POST_TYPE_CHOICES, null=True)
